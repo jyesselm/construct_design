db6623949c4aebf01017b41953ec8100c3bc7944
Joseph Yesselman <jyesselm@unl.edu>
2023-08-25 12:31:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# construct_design
+
+[![PYPI status]( https://badge.fury.io/py/construct_design.png)](http://badge.fury.io/py/construct_design)[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+Collecting a set of common tools I use often for construct design of RNA
+
+## Install
+
+To install construct_design 
+
+```shell
+python -m pip install git+https://github.com/jyesselm/construct_design
+```
+
+
+#Features
+
+## TODO
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    readme = f.read()
+
+with open("requirements.txt", "r", encoding="UTF-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='construct_design',
+    version='0.1.0',
+    description='Collecting a set of common tools I use often for construct design of RNA',
+    long_description=readme,
+    long_description_content_type="test/markdown",
+    author='Joe Yesselman',
+    author_email='jyesselm@unl.edu',
+    url='https://github.com/jyesselm/construct_design',
+    packages=[
+        'construct_design',
+    ],
+    package_dir={'construct_design': 'construct_design'},
+    py_modules=[
+        'construct_design/cli'
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='construct_design',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    entry_points = {
+        'console_scripts' : [
+        ]
+    }
+)
